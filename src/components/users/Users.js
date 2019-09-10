c2976bba12b6c6883d582d5d36da5118db5301c9
boris94kg <boris94serbia@gmail.com>
2019-09-11 00:08:24 +0200

    User component added and testing with map HFO done

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,15 +1,47 @@
 import React, {Component } from 'react'
-
+import UserItem from './UserItem'
 
 class Users extends Component{
   state = {
-
+    users: [
+      {
+        id: '1',
+        login: 'mojombo',
+        avatar_url : "https://avatars0.githubusercontent.com/u/1?v=4", 
+        html_url : "https://github.com/mojombo"
+      },
+      {
+        id: '2',
+        login: 'mojombo',
+        avatar_url : "https://avatars0.githubusercontent.com/u/1?v=4", 
+        html_url : "https://github.com/mojombo"
+      },{
+        id: '3',
+        login: 'mojombo',
+        avatar_url : "https://avatars0.githubusercontent.com/u/1?v=4", 
+        html_url : "https://github.com/mojombo"
+      }
+    ]
   }
 
   render(){
+    const {users} = this.state;
     return(
-      <div>
+      <div style={userStyle}>
+        {users.map((user) => {
+          return <UserItem key={user.id} user={user}/>
+        })}
       </div>
     )
   }
 }
+
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem'
+
+}
+
+
+export default Users
