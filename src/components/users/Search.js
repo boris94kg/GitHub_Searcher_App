69bf8875f191cd0943616dace370b2f2759870af
boris94kg <boris94serbia@gmail.com>
2019-09-18 22:34:24 +0200

    Refactoring Search component from class based to functional and started to use hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,42 +1,28 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
-class Search extends Component {
+const Search = ({searchUsers, showClear, setAlert, clearUsers}) => {
 
-  state = {
-    text: '',
-  };
+const [text, setText] = useState('');
 
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired
-  }
-
-  onChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    })
-  }
-
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (this.state.text === '') {
-      this.props.setAlert('Please enter something', 'light');
+    if (text === '') {
+      setAlert('Please enter something', 'light');
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: '' })
+      searchUsers(text);
+      setText('');
     }
-
   }
 
-  render() {
-    const { showClear, clearUsers } = this.props;
+  const onChange = (e) => {
+    setText(e.target.value);
+  }
+
     return (
       <div>
-        <form onSubmit={this.onSubmit} className="form">
-          <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange} />
+        <form onSubmit={onSubmit} className="form">
+          <input type="text" name="text" placeholder="Search Users..." value={text} onChange={onChange} />
           <input
             type="submit"
             value="Search"
@@ -47,7 +33,13 @@ class Search extends Component {
           <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
       </div>
     )
-  }
+}
+
+ Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired
 }
 
 export default Search;
